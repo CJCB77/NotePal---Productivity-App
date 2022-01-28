827d624d46e2f6bc450ef088ce5192f544307b7a
CJCB77 <coral.johnny.2@gmail.com>
2022-01-27 23:35:26 -0500

    Styled to-do-list like my wireframe

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,26 +11,38 @@ export default function ToDo() {
     return(
        <div className="todo">
             <form className="todo__form" onSubmit={handleSubmit}>
-                <input type="text" name="item" />
+                <input type="text" name="item" placeholder="Add item to list..."/>
                 <button>+</button>
             </form>
             <main className="todo__list">
 
                 <div className="todo__list--item">
-                    <input type="checkbox" name=""/>
-                    <label>Take my pet for a walk</label>
+                    <input type="checkbox" checked={false} id="item1"  name=""/>
+                    <label htmlFor="item1">Take my pet for a walk</label>
                 </div>
                 <div className="todo__list--item">
-                    <input type="checkbox" name=""/>
+                    <input type="checkbox" checked={false} name=""/>
                     <label>Read the Power of Habit</label>
                 </div>
                 <div className="todo__list--item">
-                    <input type="checkbox" name=""/>
+                    <input type="checkbox" checked={false} name=""/>
                     <label>Watch React Tutorial</label>
                 </div>
                 <div className="todo__list--item">
-                    <input type="checkbox" name=""/>
+                    <input type="checkbox" checked={false} name=""/>
                     <label>Finish my wireframe</label>
+                </div>
+                <div className="todo__list--item">
+                    <input type="checkbox" checked={false} name=""/>
+                    <label>Go buy some milk</label>
+                </div>
+                <div className="todo__list--item">
+                    <input type="checkbox" checked={false} name=""/>
+                    <label>Take a 30 min nap</label>
+                </div>
+                <div className="todo__list--item">
+                    <input type="checkbox" checked={false} name=""/>
+                    <label>Program my panel</label>
                 </div>
 
             </main>
